f13279d85663c5e24bade9e108b04b622925e917
Lessorus <urmatzarlykov555@gmail.com>
2026-02-18 02:52:15 +0700

    step 5

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ### Hexlet tests and linter status:
+<<<<<<< HEAD
 [![Actions Status](https://github.com/Lessorus/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Lessorus/frontend-project-46/actions)
+=======
+[![Actions Status](https://github.com/Lessorus/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Lessorus/frontend-project-46/actions)
+
+
+[![CI](https://github.com/Lessorus/frontend-project-46/actions/workflows/ci.yml/badge.svg)](https://github.com/Lessorus/frontend-project-46/actions/workflows/ci.yml)
+>>>>>>> 3e32cfb (step 5)
---
+++ __tests__/index.test.js
@@ -0,0 +1,22 @@
+import { test, expect, describe } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+describe('gendiff', () => {
+  test('genDiff should return correct diff for flat JSON', () => {
+    const path1 = getFixturePath('file1.json');
+    const path2 = getFixturePath('file2.json');
+    const expected = readFile('expected.txt').trim();
+
+    const result = genDiff(path1, path2);
+    expect(result).toBe(expected);
+  });
+});
